0aff85f8b62708769b60d6c43218b9797dd66c29
Jacob Rodriguez <148935424+Jacout@users.noreply.github.com>
2024-08-11 22:49:33 -0600

    Scripts V2

---
+++ CovidCount.py
@@ -0,0 +1,57 @@
+#import matplotlib.pyplot as plt
+#import requests
+#import json
+#import openpyxl
+import mod_data
+import mod_estadistica
+
+
+# Función para el menú
+def menu():
+    print("Menú:")
+    print("1. Obtener datos de la API y guardarlos en un archivo JSON")
+    print("2. Cargar datos desde un archivo JSON")
+    print("3. Generar gráfico de datos")
+    print("4. Guardar datos en un archivo Excel")
+    print("5. Calcular estadísticas")
+    print("6. Salir")
+
+def main():
+    data = None
+    while True:
+        menu()
+        opcion = input("Seleccione una opción: ")
+        if opcion == "1":
+            data = mod_data.obtener_datos()
+            if data:
+                mod_data.guardar_datos_en_json(data, "covid_data.json")  # Se puede cambiar el nombre de archivo
+        elif opcion == "2":
+            data = mod_data.cargar_datos_desde_json("covid_data.json")  # Se puede cambiar el nombre de archivo
+        elif opcion == "3":
+          print("¿Que tipo de grafica quiere generar?\n")
+          print("1. Lineal")
+          print("2. Barras Vertical")
+          print("3. Barras Horizontal")
+          tipoG = int(input())
+          if tipoG == 1:
+            mod_estadistica.generar_grafico_lineal(data)
+          elif tipoG == 2:
+            mod_estadistica.generar_grafica_barras(data)
+          elif tipoG == 3:
+            mod_estadistica.generar_grafica_barras_horizontal(data)
+          else:
+            print("Opcion no valida")
+        elif opcion == "4":
+            mod_data.guardar_datos_en_excel(data, "covid_data.xlsx")  # Se puede cambiar el nombre de archivo
+        elif opcion == "5":
+            estadisticas = mod_estadistica.calcular_estadisticas(data)
+            if estadisticas:
+                for key, value in estadisticas.items():
+                    print(f"{key}: {value}")
+        elif opcion == "6":
+            break
+        else:
+            print("Opción no válida. Intente de nuevo.")
+
+if __name__ == "__main__":
+    main()
---
+++ mod_estadistica.py
@@ -0,0 +1,100 @@
+import matplotlib.pyplot as plt
+import numpy as np
+#modulos para generar archivos
+
+#agregar solo statics para sacar media de que numero de casos se repitio mas
+
+
+# Función para generar el gráfico lineal opcion
+def generar_grafico_lineal(data):
+    if data:
+        dates = list(data['cases'].keys())
+        cases = list(data['cases'].values())
+        deaths = list(data['deaths'].values())
+        recovered = list(data['recovered'].values())
+        plt.figure(figsize=(12, 6))
+        plt.plot(dates, cases, label='Casos', marker='o')
+        plt.plot(dates, deaths, label='Muertes', marker='x')
+        plt.plot(dates, recovered, label='Recuperados', marker='s')
+        plt.xlabel('Fecha')
+        plt.ylabel('Cantidad')
+        plt.title('Evolución de casos, muertes y recuperados')
+        plt.xticks(range(0, len(dates), 30), rotation=45)  # Mostrar una etiqueta por mes
+        plt.legend()
+        plt.grid()
+        plt.tight_layout()
+        plt.show()
+
+#Grafico de barras opcion 2
+def generar_grafica_barras(data):
+    if data:
+        dates = list(data['cases'].keys())
+        cases = list(data['cases'].values())
+        deaths = list(data['deaths'].values())
+        recovered = list(data['recovered'].values())
+        plt.figure(figsize=(12, 6))
+        plt.bar(dates, cases, label='Casos')
+        plt.bar(dates, deaths, label='Muertes', alpha=0.7)
+        plt.bar(dates, recovered, label='Recuperados', alpha=0.7)
+        plt.xlabel('Fecha')
+        plt.ylabel('Cantidad')
+        plt.title('Evolución de casos, muertes y recuperados')
+        plt.xticks(range(0, len(dates), 30), rotation=45)  # Mostrar una etiqueta por mes
+        plt.legend()
+        plt.grid()
+        plt.tight_layout()
+        plt.show()
+
+#Grafico de barra horizontal para mostrar acumulado
+def generar_grafica_barras_horizontal(data):
+  if data:
+    dates = list(data['cases'].keys())
+    cases = list(data['cases'].values())
+    deaths = list(data['deaths'].values())
+    recovered = list(data['recovered'].values())
+    acumulado_cases = sum(cases)
+    acumulado_deaths = sum(deaths)
+    acumulado_recovered = sum(recovered)
+    date_x = np.array([ "Casos", "Muertes", "Recuperados"])
+    date_y = np.array([acumulado_cases, acumulado_deaths, acumulado_recovered])
+    plt.figure(figsize=(12, 6))
+    plt.barh(date_x, date_y, color="blue")
+    plt.show()
+ 
+
+# Función para realizar cálculos estadísticos
+def calcular_estadisticas(data):
+    if data:
+        cases = list(data['cases'].values())
+        deaths = list(data['deaths'].values())
+        recovered = list(data['recovered'].values())
+        total_cases = sum(cases)
+        total_deaths = sum(deaths)
+        total_recovered = sum(recovered)
+        max_cases = max(cases)
+        max_deaths = max(deaths)
+        max_recovered = max(recovered)
+        min_cases = min(cases)
+        min_deaths = min(deaths)
+        min_recovered = min(recovered)
+        promedio_cases = total_cases / len(cases)
+        promedio_deaths = total_deaths / len(deaths)
+        promedio_recovered = total_recovered / len(recovered)
+      
+        
+        return {
+            "Total de casos": total_cases,
+            "Total de muertes": total_deaths,
+            "Total de recuperados": total_recovered,
+            "Máximo de casos en un día": max_cases,
+            "Máximo de muertes en un día": max_deaths,
+            "Máximo de recuperados en un día": max_recovered,
+            "Mínimo de casos en un día": min_cases,
+            "Mínimo de muertes en un día": min_deaths,
+            "Mínimo de recuperados en un día": min_recovered,
+            "Promedio de casos por día": promedio_cases,
+            "Promedio de muertes por día": promedio_deaths,
+            "Promedio de recuperados por día": promedio_recovered
+        }
+    else:
+        return None
